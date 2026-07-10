b0445a55c4e795745ba4ad588964b114dce2faf2
Computhink-CS142 <cs142@computhink.com.sg>
2026-07-10 10:53:02 +0000

    Jul 10, 2026, 10:53 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -18,7 +18,7 @@ function draw() {
 // fill("green")
 // rect(500,120,500,500)
 fill("blue")
-trangle()
+triangle()
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
