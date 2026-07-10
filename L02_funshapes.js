5326b6d3aa8bad964440c6897f8925d1c664a6b7
Computhink-CS142 <cs142@computhink.com.sg>
2026-07-10 10:22:17 +0000

    Jul 10, 2026, 10:22 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,7 +8,7 @@ background("skyblue");
 function draw() {
   // Challenge 1: An eye using ellipse() function
 fill("blue")
-ellipse()
+ellipse(200)
 
   // Challenge 2: A face using circle() function
 
