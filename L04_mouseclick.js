804069ac70736c33e894835df159a931bdaf6557
Computhink-CS142 <cs142@computhink.com.sg>
2026-07-24 10:10:01 +0000

    Jul 24, 2026, 10:10 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,16 +1,17 @@
-function setup() {
-    createCanvas(1534, 727);
-    background("Blue")
-}
+// function setup() {
+//     createCanvas(1534, 727);
+//     background("Blue")
+// }
 
-let x1=167
-let y1=369
-let colour=0
+// let x1=167
+// let y1=369
+// let colour=0
 
-function draw(){
-    for(let x1= 0;x1<5;x1++){
-        fill(0,colour,0)
-        circle(x1,y1,100)
-        
-    }
-}
+// function draw(){
+//     for(let x1= 0;x1<5;x1++){
+//         fill(0,colour,0)
+//         circle(x1,y1,100)
+
+//     }
+// }
+g
