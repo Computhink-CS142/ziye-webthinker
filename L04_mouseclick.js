7040008bb135ed3195cb6e1dfafa0875accf4641
Computhink-CS142 <cs142@computhink.com.sg>
2026-07-24 10:17:01 +0000

    Jul 24, 2026, 10:17 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -11,9 +11,9 @@ function draw(){
     for(let x1= 0;x1<5;x1++){
         fill(0,colour,0)
         circle(x1,y1, )
-        x1+=50
-        y1+=50
-        colour+=50
+        x1+=50;
+        y1+=50;
+        colour+=50;
     }
 }
 
