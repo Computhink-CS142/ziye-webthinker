39e771c9eaa73ae39ffce98a5314c2293ba275f1
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-07 10:43:10 +0000

    Aug 7, 2026, 10:43 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -2,7 +2,7 @@ let picoImg;
 let sounndSFX
 function preload(){
     picoImg=loadImage('assets/pico-a.png');
-    soundSFX=loadSound(assets/star_wars_theme_8_bit.mp3)
+    soundSFX=loadSound(assets/star_wars_theme_8_bit.mp3.png)
 }
 
 let x;
