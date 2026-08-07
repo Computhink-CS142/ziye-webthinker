8eddf98b59f6e2fd791820ee985985c275ce2ab9
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-07 10:17:56 +0000

    Aug 7, 2026, 10:17 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,4 +1,4 @@
-
+function preload(){}
 
 let x;
 let y;
@@ -10,24 +10,24 @@ function setup() {
     x = width/2;
     y = height/2;
 }
-function draw(){
-    background("Blue")
+// function draw(){
+//     background("Blue")
 
-    fill("lightblue")
-    rect(x,y,50,50)
+//     fill("lightblue")
+//     rect(x,y,50,50)
     
-    if (keyIsDown(UP_ARROW)){
-        y=y-80;
-    }        
-    else if (keyIsDown(DOWN_ARROW)){
-        y=y+80;
-    }
-    else if (keyIsDown(RIGHT_ARROW)){
-        x=x+80;
-    }
-    else if (keyIsDown(LEFT_ARROW)){
-        x=x-80;
-    }
-    x=constrain(x,0,1534)
-    y=constrain(y,0,727)
-}
+//     if (keyIsDown(UP_ARROW)){
+//         y=y-80;
+//     }        
+//     else if (keyIsDown(DOWN_ARROW)){
+//         y=y+80;
+//     }
+//     else if (keyIsDown(RIGHT_ARROW)){
+//         x=x+80;
+//     }
+//     else if (keyIsDown(LEFT_ARROW)){
+//         x=x-80;
+//     }
+//     x=constrain(x,0,1534)
+//     y=constrain(y,0,727)
+// }
