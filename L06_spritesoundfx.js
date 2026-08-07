f8cd6f53a095cbca6e499ba8c8006759eae059c9
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-07 09:58:16 +0000

    Aug 7, 2026, 9:58 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -8,8 +8,5 @@ function draw(){
     rect(width/2,height/2,50,50)
     x=constrain(x,0,1534)
     y=constrain(y,0,727)
-    if (keyIsDown(UP_ARROW))
-        Y=Y+5
-    if (keyIsDown(DOWN_ARROW))
-        Y=Y-5
+    CZZZ
 }
