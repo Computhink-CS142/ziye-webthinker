ecf6fb1958cc3220512c8ee0eb98110886f68249
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-07 10:23:37 +0000

    Aug 7, 2026, 10:23 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -7,13 +7,13 @@ function preload(){
 // let y;
 
 function setup() {
-    createCanvas(200, 727);
+    createCanvas(200, 200);
     background("Blue");
     // rectMode(CENTER);
     // x = width/2;
     // y = height/2;
     noStroke();
-    Image(picoImg,height/2,width/2,130,150);
+    image(picoImg,height/2,width/2,130,150);
 }
 // function draw(){
 //     background("Blue")
