4238de42e544127abbdf6fde579aec0cdc818c5e
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-07 09:53:12 +0000

    Aug 7, 2026, 9:53 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -6,7 +6,7 @@ function setup() {
 function draw(){
     fill("lightblue")
     rect(width/2,height/2,50,50)
-    x=constrain(x,0,1534)
+    x=constrain(x,y,0,1534)
     y=constrain(y,0,727)
-    
+
 }
