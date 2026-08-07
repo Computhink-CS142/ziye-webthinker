25c3b97004fb4aba7d4348191cd8f306f32e983d
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-07 10:30:43 +0000

    Aug 7, 2026, 10:30 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -3,36 +3,37 @@ function preload(){
     picoImg=loadImage('assets/pico-a.png');
 }
 
-// let x;
-// let y;
+let x;
+let y;
 
 function setup() {
-    createCanvas(1400, 727);
+    createCanvas(1500, 727);
     background("Blue");
     // rectMode(CENTER);
-    // x = width/2;
-    // y = height/2;
+    x = width/2;
+    y = height/2;
     noStroke();
     image(picoImg,width/2,height/2,110,133);
 }
-// function draw(){
-//     background("Blue")
+function draw(){
+    background("Blue")
+    image(picoImg,x,y,110,133);
+    // fill("light blue")
 
-//     fill("lightblue")
 //     rect(x,y,50,50)
     
-//     if (keyIsDown(UP_ARROW)){
-//         y=y-80;
-//     }        
-//     else if (keyIsDown(DOWN_ARROW)){
-//         y=y+80;
-//     }
-//     else if (keyIsDown(RIGHT_ARROW)){
-//         x=x+80;
-//     }
-//     else if (keyIsDown(LEFT_ARROW)){
-//         x=x-80;
-//     }
+    if (keyIsDown(UP_ARROW)){
+        y=y-80;
+    }        
+    else if (keyIsDown(DOWN_ARROW)){
+        y=y+80;
+    }
+    else if (keyIsDown(RIGHT_ARROW)){
+        x=x+80;
+    }
+    else if (keyIsDown(LEFT_ARROW)){
+        x=x-80;
+    }
 //     x=constrain(x,0,1534)
 //     y=constrain(y,0,727)
-// }
+}
