00512590130ea8ec66edcada94ba71cdeca5d3d2
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-28 10:26:38 +0000

    Aug 28, 2026, 10:26 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -19,21 +19,21 @@
 //          speed=speed*-1
 //  }
 //  }
-// // let notx
+// let notx
 
 
-// // function setup(){
-// //     createCanvas(1534,720);
-// //     background("skyblue");    
-// //     textAlign(CENTER,CENTER);
-// //     textSize(100);
-// //     notx=width/2
-// // }
-// // function draw(){
-// //     background("skyblue");    
-// //     let sour=nf(hour(),2);
-// //     let zinute=nf(minute(),2);
-// //     let zecond=nf(second(),2);
-// //     let nottime=sour+":"+zinute+":"+zecond+":"
-// //     text(nottime,notx,height/2)
-// // }
+// function setup(){
+//     createCanvas(1534,720);
+//     background("skyblue");    
+//     textAlign(CENTER,CENTER);
+//     textSize(100);
+//     notx=width/2
+// }
+// function draw(){
+//     background("skyblue");    
+//     let sour=nf(hour(),2);
+//     let zinute=nf(minute(),2);
+//     let zecond=nf(second(),2);
+//     let nottime=sour+":"+zinute+":"+zecond+":"
+//     text(nottime,notx,height/2)
+// }
