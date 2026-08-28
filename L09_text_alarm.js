f985f66f9dd6d59a217c5bf1ab88adfa518649d4
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-28 10:20:41 +0000

    Aug 28, 2026, 10:20 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,39 +1,39 @@
+// // let notx
+// // let speed
+
+// // function setup(){
+// //     createCanvas(1534,720);
+// //     background("skyblue");    
+// //     textAlign(RIGHT,CENTER);
+// //     textsize(100);
+// //     notx=width/2
+// //     speed=50
+// // }
+// // function draw(){
+// //     text("bounce!",x,height);
+// //     notx=notx+speed;
+// //     if (x>width) {
+// //         speed=speed*-1
+// //     }
+// //      if (notx>0) {
+// //         speed=speed*-1
+// // }
+// // }
 // let notx
-// let speed
+
 
 // function setup(){
 //     createCanvas(1534,720);
 //     background("skyblue");    
-//     textAlign(RIGHT,CENTER);
-//     textsize(100);
+//     textAlign(CENTER,CENTER);
+//     textSize(100);
 //     notx=width/2
-//     speed=50
 // }
 // function draw(){
-//     text("bounce!",x,height);
-//     notx=notx+speed;
-//     if (x>width) {
-//         speed=speed*-1
-//     }
-//      if (notx>0) {
-//         speed=speed*-1
+//     background("skyblue");    
+//     let sour=nf(hour(),2);
+//     let zinute=nf(minute(),2);
+//     let zecond=nf(second(),2);
+//     let nottime=sour+":"+zinute+":"+zecond+":"
+//     text(nottime,notx,height/2)
 // }
-// }
-let notx
-
-
-function setup(){
-    createCanvas(1534,720);
-    background("skyblue");    
-    textAlign(CENTER,CENTER);
-    textSize(100);
-    notx=width/2
-}
-function draw(){
-    background("skyblue");    
-    let sour=nf(hour(),2);
-    let zinute=nf(minute(),2);
-    let zecond=nf(second(),2);
-    let nottime=sour+":"+zinute+":"+zecond+":"
-    text(nottime,notx,height/2)
-}
