b092e6758df237a7769de69578b3efab2d1a0706
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-28 10:03:44 +0000

    Aug 28, 2026, 10:03 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,21 +1,21 @@
-let x
-let speed
+// let x
+// let speed
 
-function setup(){
-    createCanvas(1534,720);
-    background("skyblue");    
-    textAline(RIGHT,CENTRE);
-    textsize(100);
-    x=width/2
-    speed=50
-}
-function draw(){
-    text("bounce!",x,height);
-    x=x+speed;
-    if (x>width) {
-        speed=speed*-1
-    }
-     if (x>0) {
-        speed=speed*-1
-}
-}
+// function setup(){
+//     createCanvas(1534,720);
+//     background("skyblue");    
+//     textAline(RIGHT,CENTRE);
+//     textsize(100);
+//     x=width/2
+//     speed=50
+// }
+// function draw(){
+//     text("bounce!",x,height);
+//     x=x+speed;
+//     if (x>width) {
+//         speed=speed*-1
+//     }
+//      if (x>0) {
+//         speed=speed*-1
+// }
+// }
