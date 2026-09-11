3896be7ac93dbc0381a94f1cd137401aeffd7022
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-11 10:39:14 +0000

    Sep 11, 2026, 10:39 AM

--- L10_name.js
+++ L10_name.js
@@ -1,37 +1,37 @@
-//  let userText="ENTER UR NAME HERE";
-//  let input;
-//  let agetext="age";
-//  let ageinput;
+ let userText="ENTER UR NAME HERE";
+ let input;
+ let agetext="age";
+ let ageinput;
 
  
-//  function setup(){
-//     createCanvas(600,600);
-//      background("skyblue"); 
-//      input=createInput("");
-//     input.position(160,520);
-//     input.input(updateText);
-//     ageinput=createInput("");
-//     ageinput.position(160,545);
-//     ageinput.input(updateageText);
-//  }
-//  function draw(){
-//     fill(0);
-//     background("skyblue");
-//     textSize(24);
-//     textAlign(CENTER,CENTER);
-//     text(userText,width/2,height/2);
-//     textSize(30);
-//     textAlign(LEFT);
-//     text("enter ur password",50,350)
+ function setup(){
+    createCanvas(600,600);
+     background("skyblue"); 
+     input=createInput("");
+    input.position(160,520);
+    input.input(updateText);
+    ageinput=createInput("");
+    ageinput.position(160,545);
+    ageinput.input(updateageText);
+ }
+ function draw(){
+    fill(0);
+    background("skyblue");
+    textSize(24);
+    textAlign(CENTER,CENTER);
+    text(userText,width/2,height/2);
+    textSize(30);
+    textAlign(LEFT);
+    text("enter ur password",50,350)
     
 
-//  }
-//  function updateText(){
-//     userText=this.value();
-//  }
-//  function updateageText(){
+ }
+ function updateText(){
+    userText=this.value();
+ }
+ function updateageText(){
 
-//  }
+ }
 
 let colorPicker
 function setup(){
