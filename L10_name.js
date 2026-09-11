71bfefd49dde92c38135f52c7752c5f770d218c5
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-11 10:45:39 +0000

    Sep 11, 2026, 10:45 AM

--- L10_name.js
+++ L10_name.js
@@ -19,7 +19,7 @@ let colorPicker
  function draw(){
      background(colorPicker.value())
     fill("White")
-    rect(10,200,560,200)
+    rect(10,10,560,200)
     fill(0);
     background("skyblue");
     textSize(24);
