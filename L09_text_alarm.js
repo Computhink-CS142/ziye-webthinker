ab665fe7b701ccbac8cb14c2172b69ef25b55421
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-11 09:55:11 +0000

    Sep 11, 2026, 9:55 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -72,7 +72,7 @@ let alarmTimer;
 function setup(){
     createCanvas(1534,720);
     bgcolor=color(220)
-    textAlignENTRE,CENTRE,CENTRE();  
+    textAlign(200,200);  
 }
  function draw(){
     background(bgcolor);
