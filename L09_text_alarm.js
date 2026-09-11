c8425ce52343d6a9e10ea2e003d8807e15fa20e6
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-11 09:57:21 +0000

    Sep 11, 2026, 9:57 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -100,48 +100,48 @@
 //     bgcolor=color(random(255),random(255),random(255))
 //     alarmSound.loop();
 //  }
-// let notx
+let notx
 
 
-// function setup(){
-//     createCanvas(1534,720);
-//     background("skyblue");    
-//     textAlign(CENTER,CENTER);
-//     textSize(100);
-//     notx=width/2
-// }
-// function draw(){
-//     background("skyblue");    
-//     let sour=nf(hour(),2);
-//     let zinute=nf(minute(),2);
-//     let zecond=nf(second(),2);
-//     let nottime=sour+":"+zinute+":"+zecond+":"
-//     text(nottime,notx,height/2)
-// }
-// let countdown=5;
-// let timer;
-// function setup(){
-//     createCanvas(1534,720);
-//     background("skyblue");    
-// }
-//  function draw(){
-//     background("skyblue");
-//     fill(0);
-//     textSize(60);
-//     text(countdown,180,200);
-//     textSize(18);
-//     text("click me to start the timer",200,250);
+function setup(){
+    createCanvas(1534,720);
+    background("skyblue");    
+    textAlign(CENTER,CENTER);
+    textSize(100);
+    notx=width/2
+}
+function draw(){
+    background("skyblue");    
+    let sour=nf(hour(),2);
+    let zinute=nf(minute(),2);
+    let zecond=nf(second(),2);
+    let nottime=sour+":"+zinute+":"+zecond+":"
+    text(nottime,notx,height/2)
+}
+let countdown=5;
+let timer;
+function setup(){
+    createCanvas(1534,720);
+    background("skyblue");    
+}
+ function draw(){
+    background("skyblue");
+    fill(0);
+    textSize(60);
+    text(countdown,180,200);
+    textSize(18);
+    text("click me to start the timer",200,250);
 
-//  }
-//  function mousePressed(){
-//     countdown=5;
-//     clearInterval(timer)
-//     timer=setInterval(countDown,1000);
-//  }
-//  function countDown(){
-//     if (countdown>0){
-//         countdown--;
-//     }else{
-//         clearInterval(timer)
-//     }
-//  }
+ }
+ function mousePressed(){
+    countdown=5;
+    clearInterval(timer)
+    timer=setInterval(countDown,1000);
+ }
+ function countDown(){
+    if (countdown>0){
+        countdown--;
+    }else{
+        clearInterval(timer)
+    }
+ }
