7329bc5522bfa4bb45451a3eb08fff9f69479c4f
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-11 11:01:24 +0000

    Sep 11, 2026, 11:01 AM

--- L10_name.js
+++ L10_name.js
@@ -12,7 +12,7 @@ let colorPicker3
     colorPicker2=createColorPicker("#ffffff");
     colorPicker2.position(160,620);
     colorPicker3=createColorPicker("#ffffff");
-    colorPicker3.position(160,620);
+    colorPicker3.position(160,660);
 
     createCanvas(600,700);
      background("skyblue"); 
