724954ed4d292015674f1616c5932841bc95f455
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:30:44 +0000

    Sep 18, 2026, 10:30 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -55,9 +55,9 @@ function draw(){
     textAlign(RIGHT,CENTER);
     textSize(18);
     text("type a noun(living thing)eg,wholphin",width/2,45)
-    text("type a verb(living thing)eg,wholphin",width/2,45)
-    text("type a adjective(living thing)eg,wholphin",width/2,45)
-    text("type a adverb(living thing)eg,wholphin",width/2,45)
+    text("type a verb()eg,wholphin",width/2,45)
+    text("type a adjective()eg,wholphin",width/2,45)
+    text("type a adverb()eg,wholphin",width/2,45)
     text("type a place(living thing)eg,wholphin",width/2,45)
 
 }
