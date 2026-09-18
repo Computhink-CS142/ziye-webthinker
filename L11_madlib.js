8cb11996bf19d6b8559a2dde7b42d562dd84daae
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:57:16 +0000

    Sep 18, 2026, 10:57 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -74,4 +74,5 @@ function updateText(){
     template=template.replace("{adjective}",adjective.value)
     template=template.replace("{adverb}",adverb.value)
     template=template.replace("{place}",place.value)
+    storyText
 }
