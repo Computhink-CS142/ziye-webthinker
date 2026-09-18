7b38ae54e1b8090130bf8aa87089e04a5fd560f7
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:29:25 +0000

    Sep 18, 2026, 10:29 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -54,5 +54,5 @@ function draw(){
     background("skyblue");
     textAlign(RIGHT,CENTER);
     textSize(18);
-    text("type a noun(living thing)eg,wholphin")
+    text("type a noun(living thing)eg,wholphin",width)
 }
