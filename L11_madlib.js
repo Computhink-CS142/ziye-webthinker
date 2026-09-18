d030f8d4b6b8039da8d619aa8fa055fa45f96539
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:50:33 +0000

    Sep 18, 2026, 10:50 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -39,7 +39,7 @@ let storyTemplates;
 function setup(){
     storyTemplates=[
         "the{adjective}{verb]decided to {} {} at the {place}.",
-        "one day,a {adjective}{} wanted to {verb} {} in {place}",
+        "one day,a {adjective}{dverb} wanted to {verb} {dverb} in {place}",
         "did you hear about the{adjective}{noun} that tried to {verb} {} near {place}"
     ]
     createCanvas(700,700);
