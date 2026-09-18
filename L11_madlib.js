83459e9f971bd22665456d84ff579c9477ab451d
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:19:35 +0000

    Sep 18, 2026, 10:19 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -43,7 +43,7 @@ function setup(){
     verb=createInput("");
     verb.position(20,20);
     verb=createInput("");
-    noun.position(20,20);
+    adjective.position(20,20);
     noun=createInput("");
     noun.position(20,20);
     noun=createInput("");
