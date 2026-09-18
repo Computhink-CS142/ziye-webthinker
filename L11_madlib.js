3e867c5adb0660d4f08697f21b9ae60140b443e3
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:08:50 +0000

    Sep 18, 2026, 10:08 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,29 +1,29 @@
-let userText="";
-let input;
-let ageinput;
-let colorPicker
+// let userText="";
+// let input;
+// let ageinput;
+// let colorPicker
  
- function setup(){
-    colorPicker=createColorPicker("black");
-    colorPicker.position(20,40);
+//  function setup(){
+//     colorPicker=createColorPicker("black");
+//     colorPicker.position(20,40);
 
-    createCanvas(1500,200);
-     background("skyblue"); 
-     input=createInput("");
-    input.position(20,20);
-    input.input(updateText);
-// 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥6️⃣7️⃣
- }
- function draw(){
-    background("skyblue");
-    fill(colorPicker.value());
-    textSize(24);
-    textAlign(CENTER,CENTER);
-    text(userText,width/2,height/2);
+//     createCanvas(1500,200);
+//      background("skyblue"); 
+//      input=createInput("");
+//     input.position(20,20);
+//     input.input(updateText);
+// // 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥6️⃣7️⃣
+//  }
+//  function draw(){
+//     background("skyblue");
+//     fill(colorPicker.value());
+//     textSize(24);
+//     textAlign(CENTER,CENTER);
+//     text(userText,width/2,height/2);
    
     
 
- }
- function updateText(){
-    userText=this.value();
- }
+//  }
+//  function updateText(){
+//     userText=this.value();
+//  }
