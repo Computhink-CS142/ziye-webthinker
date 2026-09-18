606aeb64ec3ac2489684e5d29ff3c34957b97843
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 09:44:48 +0000

    Sep 18, 2026, 9:44 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -7,7 +7,7 @@ let colorPicker3
  
  function setup(){
     colorPicker=createColorPicker("#0055ff");
-    colorPicker.position(160,180);
+    colorPicker.position(100,100);
     colorPicker2=createColorPicker("#ffffff");
     colorPicker2.position(160,620);
     colorPicker3=createColorPicker("black");
