39f73b862a27be7a05df999555764580f19a78bb
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:32:39 +0000

    Sep 18, 2026, 10:32 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -58,6 +58,6 @@ function draw(){
     text("type a verb()eg,wholphin",width/2,45)
     text("type a adjective()eg,wholphin",width/2,45)
     text("type a adverb()eg,wholphin",width/2,45)
-    text("type a place(somewere,a city,aplace,a country)eg,wholphin",width/2,45)
+    text("type a place(somewere,a city,aplace,a country)eg,United Kingdom of Great Britain and Northern Ireland",width/2,45)
 
 }
