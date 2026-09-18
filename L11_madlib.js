4d809a5ae9e425e39c95260b6adf2e0e05620b34
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 11:00:26 +0000

    Sep 18, 2026, 11:00 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -62,8 +62,8 @@ function draw(){
     textSize(18);
     text("type a noun(living thing)eg,wholphin",width/2,45)
     text("type a verb(action)eg,floccinaucinihilipilificating",width/2,75)
-    text("type a adjective(a felling)eg,Supercalifragilisticexpialidocious ",width/2,415)
-    text("type a adverb(a feeling a extreme expression)eg,spectrophotofluorometrically",width/2,45)
+    text("type a adjective(a felling)eg,Supercalifragilisticexpialidocious ",width/2,105)
+    text("type a adverb(a feeling a extreme expression)eg,spectrophotofluorometrically",width/2,135)
     text("type a place(somewere,a city,aplace,a country)eg,United Kingdom of Great Britain and Northern Ireland",width/2,45)
 
 }
