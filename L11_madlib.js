b747098b4520388a51d5a9b69f1048873c928329
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 09:51:38 +0000

    Sep 18, 2026, 9:51 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -5,7 +5,7 @@ let colorPicker
  
  function setup(){
     colorPicker=createColorPicker("black");
-    colorPicker.position(160,660);
+    colorPicker.position(20,20);
 
     createCanvas(200,200);
      background("skyblue"); 
