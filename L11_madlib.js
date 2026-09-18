05aa7d71711bc52a5e01a41e54d834e6d8e9569b
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:43:14 +0000

    Sep 18, 2026, 10:43 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -38,7 +38,7 @@ let storyText;
 let storyTemplates;
 function setup(){
     storyTemplates=[
-        "the{}"
+        "the{adjective}"
     ]
     createCanvas(700,700);
     background("skyblue"); 
