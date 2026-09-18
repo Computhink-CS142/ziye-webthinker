7a8e231ce7a953cee54fdb511d96736572ce7fa0
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-18 10:39:35 +0000

    Sep 18, 2026, 10:39 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -45,7 +45,7 @@ function setup(){
     adjective=createInput("");
     adjective.position(width/2+20,95);
     adverb=createInput("");
-    adverbcsssssss.position(width/2+20,125);
+    adverb.position(width/2+20,125);
     storyButton=createInput("create a story!!!");
     storyButton.position(20,20);
     storyButton.mousePressed(updateText);
