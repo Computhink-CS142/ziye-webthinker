d0a0d661b1983938b04faab978040cc7f5453010
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 09:44:34 +0000

    Sep 25, 2026, 9:44 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -67,7 +67,7 @@ function draw(){
     text("type a place(somewere,a city,aplace,a country)eg,United Kingdom of Great Britain and Northern Ireland",width/2,165)
     textAlign(CENTER,CENTER )
     FileList("red")
-    text(my)
+    text(myStory,width)
 }
 function updateText(){
     template=random(storyTemplates)
