c6d07aff832ea395f1cd53054499c5fd4cae02bc
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 10:14:46 +0000

    Sep 25, 2026, 10:14 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,7 +1,12 @@
 function setup(){
 createCanvas(800,700);
-background("lightgrey")
+background("lightgrey");
+place=createInput("");
+    place.position(width/2+20,155);
+    storyButton=createButton("create a story!!!");
+    storyButton.position(width/2+20,185);
+    storyButton.mousePressed(updateText);
 }
 function draw(){
-    background("lightgrey")
+    background("lightgrey");
 }
