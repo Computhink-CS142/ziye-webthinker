7d657e7d9b8ba972014a36c4b5801a1ce6300668
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 10:18:26 +0000

    Sep 25, 2026, 10:18 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -4,7 +4,7 @@ function setup(){
     createCanvas(800,700);
     background("lightgrey");
     guesstype=createInput("");
-    guesstype.position(width/2+20,155);
+    guesstype.position(width/2,);
     guessbutton=createButton("?guess?");
     guessbutton.position(width/2+20,185);
     guessbutton.mousePressed(updateText);
