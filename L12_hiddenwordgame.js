eef564f420f2d6d08970024986d3f7e25e4edab4
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 10:47:13 +0000

    Sep 25, 2026, 10:47 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,5 +1,5 @@
 let guessbutton
-let guesstype
+let guessword
 let atmpt=0
 let hints
 let wordlist
@@ -7,7 +7,7 @@ function setup(){
     wordlist=["alarm", "angry", "arrow", "avoid", "badge", "basic", "below", "bench", "blame", "blind", "block", "blood", "board", "boast", "brain", "bread", "break", "brick", "brief", "bring", "broad", "brush", "cabin", "cable", "camel", "candy", "carry", "carve", "cause", "chain", "chalk", "charm", "chase", "cheap", "cheat", "check", "cheek", "cheer", "chest", "chief", "child", "chill", "choke", "chore", "claim", "clash", "class", "clean", "clear", "climb", "cloak", "clock", "close", "cloth", "cloud", "clown", "coach", "coast", "color", "cough", "count", "court", "cover", "crack", "craft", "crane", "crash", "crate", "crawl", "crazy", "cream", "crime", "crisp", "cross", "crowd", "crown", "cruel", "crumb", "crush", "crust", "curve", "cycle"]
     createCanvas(1000,700);
     background("lightgrey");
-    guesstype=createInput("");
+    guessword=createInput("");
     guesstype.position(300,400);
     guessbutton=createButton("?guess?");
     guessbutton.position(width/2+25,400);
