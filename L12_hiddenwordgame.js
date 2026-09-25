610e682f00cf50b769dd885168eb3f9650d0e264
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 10:23:12 +0000

    Sep 25, 2026, 10:23 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -11,7 +11,7 @@ function setup(){
 }
 function draw(){
     background("lightgrey");
-    text("GUESS THE HIDDEN WORD!")
+    text("GUESS THE HIDDEN WORD!",)
     textSize(100)
 }
 function updateText(){
