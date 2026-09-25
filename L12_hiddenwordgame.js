898de4be7b22e8b3a8b123ff9edb40508ec4ce50
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 10:44:33 +0000

    Sep 25, 2026, 10:44 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -4,7 +4,7 @@ let atmpt=0
 let hints
 let wordlist
 function setup(){
-    wordlist=["alarm", "angry", "arrow", "avoid", "awful", "badge", "basic", "below", "bench", "blame", "blind", "block", "blood", "board", "boast", "brain", "bread", "break", "brick", "brief", "bring", "broad", "brush", "cabin", "cable", "camel", "candy", "carry", "carve", "cause", "chain", "chalk", "charm", "chase", "cheap", "cheat", "check", "cheek", "cheer", "chest", "chief", "child", "chill", "choke", "chore", "claim", "clash", "class", "clean", "clear", "climb", "cloak", "clock", "close", "cloth", "cloud", "clown", "coach", "coast", "color", "cough", "count", "court", "cover", "crack", "craft", "crane", "crash", "crate", "crawl", "crazy", "cream", "crime", "crisp", "cross", "crowd", "crown", "cruel", "crumb", "crush", "crust", "curve", "cycle"]
+    wordlist=["alarm", "angry", "arrow", "avoid", "badge", "basic", "below", "bench", "blame", "blind", "block", "blood", "board", "boast", "brain", "bread", "break", "brick", "brief", "bring", "broad", "brush", "cabin", "cable", "camel", "candy", "carry", "carve", "cause", "chain", "chalk", "charm", "chase", "cheap", "cheat", "check", "cheek", "cheer", "chest", "chief", "child", "chill", "choke", "chore", "claim", "clash", "class", "clean", "clear", "climb", "cloak", "clock", "close", "cloth", "cloud", "clown", "coach", "coast", "color", "cough", "count", "court", "cover", "crack", "craft", "crane", "crash", "crate", "crawl", "crazy", "cream", "crime", "crisp", "cross", "crowd", "crown", "cruel", "crumb", "crush", "crust", "curve", "cycle"]
     createCanvas(1000,700);
     background("lightgrey");
     guesstype=createInput("");
