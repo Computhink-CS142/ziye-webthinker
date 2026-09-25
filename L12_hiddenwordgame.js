326fd69752cc73ce81ece6f954fdab98a624bac7
Computhink-CS142 <cs142@computhink.com.sg>
2026-09-25 10:57:42 +0000

    Sep 25, 2026, 10:57 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -28,7 +28,7 @@ function draw(){
 
     textSize(50);
     text("Attempts:"+atmpt,300,300);
-    text("Hint:",300,350)
+    text("Hint:"+wo,300,350)
 }
 function updateButton(){
     print("hello")
